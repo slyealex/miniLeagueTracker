02f0f27c594e814bfd66e5aa34800860748f5e5d
ALEX SLYE <slyealex@live.com>
2016-08-11 21:01:17 -0400

    add current_user helper method to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def ensure_logged_in
+    unless logged_in
+      redirect_to new_session_path, notice: "please log in"
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+  
 end

d5c3385d5cec260782c98a0231415397ab58afe1
ALEX SLYE <slyealex@live.com>
2016-08-15 12:49:42 -0400

    add forms for user sign up, add form for new user session

--- app/controllers/aos_leagues_controller.rb
+++ app/controllers/aos_leagues_controller.rb
@@ -1,10 +1,23 @@
 class AosLeaguesController < ApplicationController
   def new
+    AosLeague.new
+  end
+
+  def create
+    @aos_league = AosLeague.new(aos_league_params)
+
+    if @aos_league.save
+      redirect_to aos_leagues_path, notice: "League created!"
+    else
+      render :new
+    end
   end
 
   def show
+    @aos_league = AosLeague.find(params[:id])
   end
 
   def index
+    @aos_leagues = AosLeague.all
   end
 end
